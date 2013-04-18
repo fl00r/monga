b4f98e0030f1a7d415239bb12d144e147e2d94b4
Petr Yanovich <fl00r@yandex.ru>
2013-04-18 18:19:25 +0400

    README

--- lib/monga/connections/fibered_connection.rb
+++ lib/monga/connections/fibered_connection.rb
@@ -24,21 +24,18 @@ module Monga::Connections
   end
 end
 
-Patching async iteration in sync mode!
+# Patching async iteration in sync mode!
 module Monga
   class Cursor
     alias_method :orig_each_batch, :each_batch
 
     def each_batch(&blk)
       begin
-        fib = Fiber.current
         new_blk = proc{ |err, batch, iter|
-          fib.resume(err, batch, iter)
+          raise err if err
+          yield batch
         }
         orig_each_batch(&new_blk)
-        err, batch, iter = Fiber.yield
-        raise err if err
-        yield batch
       end while iter
     end
   end
