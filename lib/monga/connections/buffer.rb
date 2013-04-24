f88d8bb7cdeb817acc666b6e14ca65c7b0be4fbf
Peter Yanovich <fl00r@yandex.ru>
2013-04-25 00:27:59 +0400

    night fixes

--- lib/monga/connections/buffer.rb
+++ lib/monga/connections/buffer.rb
@@ -14,17 +14,17 @@ module Monga::Connections
     def append(data)
       @buffer << data
       @buffer_size = @buffer.bytesize
-      job
+      parse
       @buffer
     end
 
-    def job
-      parse_meta if @position == 0
-      parse_doc if @position > 0
+    def parse
+      parse_meta  if @position == 0
+      parse_doc  if @position > 0
     end
 
     def parse_meta
-      return if @buffer_size < 36
+      return  if @buffer_size < 36
       @response = []
       @response << ::BinUtils.get_int32_le(@buffer, @position)
       @response << ::BinUtils.get_int32_le(@buffer, @position += 4)
@@ -41,21 +41,15 @@ module Monga::Connections
 
     def parse_doc
       while true
-        if @number_returned == 0
-          done
-          break
-        end
-        break if @buffer_size < @position + 4
+        break  if done
+        break  if @buffer_size < @position + 4
         doc_length = ::BinUtils.get_int32_le(@buffer, @position)
-        break if @buffer_size < @position + doc_length
+        break  if @buffer_size < @position + doc_length
+
         doc = @buffer[@position, doc_length]
         @response[-1] << CBson.deserialize(doc)
         @position += doc_length
         @number_returned -= 1
-        if @number_returned == 0
-          done
-          break
-        end
       end
     end
 
@@ -66,6 +60,7 @@ module Monga::Connections
     end
 
     def done
+      return false  if @number_returned > 0
       @responses << @response
       @response = nil
       if @buffer_size == @position
